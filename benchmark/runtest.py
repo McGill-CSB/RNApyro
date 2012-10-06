6ee747a1a49b4a5077737befa6108cfaf3046c2f
Jerome Waldispuhl <waldispuhl@gmail.com>
2012-10-06 02:50:18 -0400

    update script & add results section (partial)

--- benchmark/runtest.py
+++ benchmark/runtest.py
@@ -2,13 +2,15 @@ import sys,os,os.path
 
 def main(nmut,alpha,prefix):
 
-    for numex in range(10):
+    for numex in range(100):
         infile = "RF00001_%d.in" % numex
         if os.path.exists(infile):
             alphanorm = int (alpha * 100)
-            commandline1="python ../src/RNAPyroEx.py RF00001_%d.in %d %.1f > RF00001_%d_nmut_%d_alpha_%d.out" % (numex,nmut,alpha,numex,nmut,alphanorm)
+            outfile = "RF00001_%d_nmut_%d_alpha_%d.out" % (numex,nmut,alphanorm)
+            commandline1="python ../src/RNAPyroEx.py %s %d %.1f 1.5 > %s" % (infile,nmut,alpha,outfile)
             print commandline1
-            os.system(commandline1)
+            if not os.path.exists(outfile):
+                os.system(commandline1)
             commandline2="python ../scripts/benchmark.py -i RF00001_%d.ref -o RF00001_%d_nmut_%d_alpha_%d.out" % (numex,numex,nmut,alphanorm)
             print commandline2
             os.system(commandline2)
@@ -22,14 +24,11 @@ if __name__ == '__main__':
     alpha=0.5
     prefix="RF00001"
     
-    if len(sys.argv)==1:
-        usage(sys.argv[0])
-    
     for i in range(len(sys.argv)):
         myarg=sys.argv[i]
         if myarg=='-h' or myarg=='--help':
             usage(sys.argv[0])
-        if myarg=='-n':
+        if myarg=='-m':
             nmut=int(sys.argv[i+1])
         if myarg=='-a':
             alpha=float(sys.argv[i+1])
